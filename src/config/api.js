561484b5fad5aacbbc80ab13c73090cb04f2424c
rishabhjaiswal3 <rj838486@gmail.com>
2026-02-20 15:36:57 +0530

    use hardcoded domain name

--- src/config/api.js
+++ src/config/api.js
@@ -2,7 +2,8 @@ const DEFAULT_API_ORIGIN = 'https://api.warzonewarriors.xyz';
 // const DEFAULT_API_ORIGIN = 'http://localhost:3300';
 const API_PATH_PREFIX = '/warzone';
 
-export const API_ORIGIN = import.meta.env.VITE_API_ORIGIN || DEFAULT_API_ORIGIN;
+// export const API_ORIGIN = import.meta.env.VITE_API_ORIGIN || DEFAULT_API_ORIGIN;
+export const API_ORIGIN = DEFAULT_API_ORIGIN;
 export const API_BASE_URL = `${API_ORIGIN}${API_PATH_PREFIX}`;
 
 export const buildApiUrl = (path = '') => {
